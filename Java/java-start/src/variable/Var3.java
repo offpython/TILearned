8385773c557790dcb11359b8da4792ff92839b12
offpython <offpython@gmail.com>
2024-07-13 17:16:34 +0900

    7/13 TIL

---
+++ Java/java-start/src/variable/Var3.java
@@ -0,0 +1,14 @@
+package variable;
+
+import java.sql.SQLOutput;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a; // 변수선언
+        a = 10; // 변수 초기화 a(10)
+        System.out.println(a);
+        a = 50; // 변수 값 변경 a(10 -> 50)
+        System.out.println(a);
+    }
+}
+ 
