19d4eedb1257c2f73a9c200fee42335bfa5d0719
offpython <offpython@gmail.com>
2024-07-14 23:50:43 +0900

    7/14 TIL

---
+++ Java/java-start/src/variable/Var4.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a; // 변수선언
+        a = 10; // 변수 초기화 a(10)
+        System.out.println(a);
+        a = 50; // 변수 값 변경 a(10 -> 50)
+        System.out.println(a);
+    }
+}
+ 
