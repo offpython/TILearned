8a085a6a74de4f7e02209ab2745cffd6c91f0d1d
offpython <offpython@gmail.com>
2024-07-19 19:19:15 +0900

    7/19 TIL

--- Java/java-start/src/loop/While2_3.java
+++ Java/java-start/src/loop/While2_3.java
@@ -3,8 +3,8 @@ package loop;
 public class While2_3 {
     public static void main(String[] args) {
         int sum = 0;
-        int i = 1;
-        int endNum = 10;
+        int i = 50;
+        int endNum = 100;
 
         while (i <= endNum){
             sum = sum + i;
