cbe002ad9c4494a2a2704c35f711cbedadb6755b
offpython <offpython@gmail.com>
2024-07-24 16:54:15 +0900

    7/24 TIL

---
+++ Java/java-start/src/scanner/ScannerWhile3.java
@@ -0,0 +1,16 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhile3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+
+        while (true){
+            System.out.print("정수를 입력하세요 (0을 입력하면 종료) : ");
+        }
+    }
+}
