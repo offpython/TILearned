fb5ca461135069235c068649db9eaa7338ddf61e
offpython <offpython@gmail.com>
2024-07-30 13:40:16 +0900

    7/30 TIL

--- Java/java-start/src/scanner/ex/ScannerWhileEx2.java
+++ Java/java-start/src/scanner/ex/ScannerWhileEx2.java
@@ -6,8 +6,23 @@ public class ScannerWhileEx2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println();
-        int price = sc.nextInt();
+        while (true){
+            System.out.println("상품의 가격을 입력하시오 (-1을 입력하면 종료) : ");
+            int price = sc.nextInt();
+
+            if (price == -1){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            System.out.println("구매하려는 수량을 입력하시오 : ");
+            int quantity  = sc.nextInt();
+
+            int totalCost = price * quantity;
+            System.out.println("총 비용 : " + totalCost);
+        }
+
+
 
     }
 }
