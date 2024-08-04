e8a7aa6847ad85d8913cb855087096d41b774421
offpython <offpython@gmail.com>
2024-08-04 15:29:32 +0900

    8/4 TIL

---
+++ Java/java-start/src/array/ArrayRef1.java
@@ -0,0 +1,23 @@
+package array;
+
+public class ArrayRef1 {
+    public static void main(String[] args) {
+        int[] student; //배열 변수 선언
+        student = new int[5];  //배열 생성
+
+        //변수 값 대입
+        student[0] = 90;
+        student[1] = 80;
+        student[2] = 70;
+        student[3] = 60;
+        student[4] = 50;
+
+        //변수 값 사용
+        System.out.println("학생1 점수 : " + student[0]);
+        System.out.println("학생2 점수 : " + student[1]);
+        System.out.println("학생3 점수 : " + student[2]);
+        System.out.println("학생4 점수 : " + student[3]);
+        System.out.println("학생5 점수 : " + student[4]);
+
+    }
+}
