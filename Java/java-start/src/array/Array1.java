3e086fd054cd7882840179a441199bd56f9a95c9
offpython <offpython@gmail.com>
2024-08-05 07:18:04 +0900

    8/5 TIL

---
+++ Java/java-start/src/array/Array1.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int student1 = 10;
+        int student2 = 20;
+        int student3 = 30;
+
+        System.out.println("학생1 점수 : " + student1);
+        System.out.println("학생2 점수 : " + student2);
+        System.out.println("학생3 점수 : " + student3);
+
+    }
+}
